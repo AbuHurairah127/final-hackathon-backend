76bf219b1775fd3142a1636ea37f33e13882705b
Abuhurairah127 <abuhurairahmuhammadshafique@gmail.com>
2022-08-29 06:48:22 +0500

    logging in user

---
+++ src/controllers/user.js
@@ -0,0 +1,83 @@
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const JWT_SECRET_KEY = process.env.SECRET_KEY;
+const User = require("../models/user");
+const { validationResult } = require("express-validator");
+const register = async (req, res) => {
+  try {
+    /* Checking if the request body has any errors. If it does, it will return a 400 status code with the
+  errors. */
+
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    /* This is checking if the email address already exists in the database. If it does, it will return a
+    400 status code with the message "Sorry a user with this email address already exists." */
+    let isEmail = await User.findOne({ email: req.body.email });
+    if (isEmail) {
+      return res
+        .status(400)
+        .json("Sorry a user with this email address already exists.");
+    }
+    let isUserName = await User.findOne({ userName: req.body.userName });
+    if (isUserName) {
+      return res
+        .status(400)
+        .json("Sorry a user with this username already exists.");
+    }
+    const salt = await bcrypt.genSaltSync(10);
+    secPass = await bcrypt.hash(req.body.password, salt);
+    const user = await User.create({
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      userName: req.body.userName,
+      password: secPass,
+      email: req.body.email,
+      phoneNumber: req.body.phoneNumber,
+    });
+    const data = {
+      user: { id: user.id },
+    };
+    const authToken = jwt.sign(data, JWT_SECRET_KEY);
+    res.json({ authToken });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json("Some error occurred");
+  }
+};
+const login = async (req, res) => {
+  try {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    const { email, password } = req.body;
+    let user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({
+        error: "Sorry you are not the user of our platform. Please register!",
+      });
+    }
+    const passwordCompare = await bcrypt.compare(password, user.password);
+    if (!passwordCompare) {
+      return res.status(400).json({ error: "Password is incorrect." });
+    }
+    const data = {
+      user: {
+        id: user.id,
+      },
+    };
+    const authToken = jwt.sign(data, JWT_SECRET_KEY);
+    res.json({
+      authToken: authToken,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json("Internal server error");
+  }
+};
+module.exports = {
+  register,
+  login,
+};
--- src/routes/user.js
+++ src/routes/user.js
@@ -1,55 +1,28 @@
 const express = require("express");
 const User = require("../models/user");
 const router = express.Router();
-const { body, validationResult } = require("express-validator");
-const bcrypt = require("bcryptjs");
-const jwt = require("jsonwebtoken");
+const { body } = require("express-validator");
+const authController = require("./../controllers/user");
+//Route -1 creating a new user:No login required
 router.post(
   "/register",
   [
-    body("email").isEmail(),
-    body("password").isLength({ min: 5 }),
-    body("name").isLength({ min: 3 }),
+    body("email", "Please enter a valid email address.").isEmail(),
+    body("password", "Password is incorrect").isLength({ min: 5 }),
+    body("userName", "Please enter the username").isLength({ min: 3 }),
+    body("lastName", "Please enter your last name").isLength({ min: 3 }),
+    body("firstName", "please enter your first name").isLength({ min: 3 }),
+    body("phoneNumber", "Please enter your phone number").isLength({ min: 11 }),
   ],
-  async (req, res) => {
-    try {
-      /* Checking if the request body has any errors. If it does, it will return a 400 status code with the
-  errors. */
-      const errors = validationResult(req);
-      if (!errors.isEmpty()) {
-        return res.status(400).json({ errors: errors.array() });
-      }
-      /* This is checking if the email address already exists in the database. If it does, it will return a
-    400 status code with the message "Sorry a user with this email address already exists." */
-      let isEmail = await User.findOne({ email: req.body.email });
-      if (isEmail) {
-        return res
-          .status(400)
-          .json("Sorry a user with this email address already exists.");
-      }
-      let isUserName = await User.findOne({ email: req.body.email });
-      if (isUserName) {
-        return res
-          .status(400)
-          .json("Sorry a user with this username already exists.");
-      }
-      const salt = await bcrypt.genSaltSync(10);
-      secPass = await bcrypt.hash(req.body.password, salt);
-      user = await User.create({
-        name: req.body.name,
-        password: secPass,
-        email: req.body.email,
-        designation: req.body.designation,
-      });
-      const data = {
-        user: { id: user.id },
-      };
-      const authToken = jwt.sign(data, JWT_SECRET_KEY);
-      res.json({ authToken });
-    } catch (error) {
-      console.error(error);
-      res.status(500).json("Some error occurred");
-    }
-  }
+  authController.register
+);
+//Route - 2 Logging in a user:No login required
+router.post(
+  "/login",
+  [
+    body("email", "Please enter a valid email address.").isEmail(),
+    body("password", "Password is incorrect").isLength({ min: 5 }),
+  ],
+  authController.login
 );
 module.exports = router;
