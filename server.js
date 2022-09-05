3380fb5a7bf353b96a329d7282507d24490d6f78
Abu Hurairah <91326530+AbuHurairah127@users.noreply.github.com>
2022-09-05 22:49:17 +0500

    Revert "wishlist deleted from a seprate file"
    This reverts commit 0d079f8b52dca68056425176c98611b0a79b8eda.

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ app.use(express.json());
 app.use(cors());
 app.use("/auth", require("./src/routes/user"));
 app.use("/properties", require("./src/routes/property"));
+app.use("/wishlist", require("./src/routes/wishlist"));
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
